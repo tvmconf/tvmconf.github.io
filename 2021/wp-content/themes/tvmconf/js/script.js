b0ec328b90cd1676d7a4181b130ecc6daac550a5
Chris Hoge <choge@octoml.ai>
2022-01-25 13:17:50 -0800

    Build complete archive of all four previous conference sites (#10)
    Convert from a Jeckyl site to a static archive of all previous sites.
    Updates to theme wound up being applied to all previous years
    sites, causing the state and some information to be lost. Additionally,
    the 2021 site was served from a different location using WordPress.
    
    This patch takes all of the sites, as they were at the time of the
    event, and makes a static archive of them. Some indexing changes
    were made, but this update should capture the state of all previous
    events and make way for new event management for 2022 and going
    forward.
    
    The new home for the conference is https://www.tvmcon.org

---
+++ 2021/wp-content/themes/tvmconf/js/script.js
@@ -0,0 +1,21 @@
+// activate hamburger menu
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+
+// close hamburger menu when link clicked
+const navLink = document.querySelectorAll("#nav-menu a");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
